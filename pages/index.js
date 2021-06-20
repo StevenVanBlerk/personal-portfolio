a7929233717d69c496977b5acf42934e6dfba910
Steven van Blerk <stevenvb@mainboard.com>
2021-06-20 12:11:56 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
+          Get started by editing TEST TEST TEST{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
